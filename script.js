d46c31910b83c68db75e5a5ca7db06451f42607c
varuna2hash <varuna2@gmail.com>
2026-02-14 23:52:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+﻿function sendMessage(){
+alert("Message Sent Successfully!");
+return false;
+}
+
+// Particle Background
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+class Particle{
+constructor(){
+this.x=Math.random()*canvas.width;
+this.y=Math.random()*canvas.height;
+this.size=Math.random()*2;
+this.speedX=(Math.random()-0.5);
+this.speedY=(Math.random()-0.5);
+}
+update(){
+this.x+=this.speedX;
+this.y+=this.speedY;
+}
+draw(){
+ctx.fillStyle="#1f6feb";
+ctx.beginPath();
+ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+ctx.fill();
+}
+}
+
+function init(){
+for(let i=0;i<100;i++){
+particles.push(new Particle());
+}
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+particles.forEach(p=>{
+p.update();
+p.draw();
+});
+requestAnimationFrame(animate);
+}
+
+init();
+animate();
